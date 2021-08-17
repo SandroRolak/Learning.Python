fd00b41b5d72e8e0a0b98e60872e8a549605df19
SandroRolak <sandro.rolak2090@gmail.com>
2021-08-17 15:41:58 -0300

    Atualizações

--- Desafio1.py
+++ Desafio1.py
@@ -1,7 +1,7 @@
 print("Qual é seu nome?")
 nome = input()
 print("Que dia da semana é hoje?")
-datetime = input()
+date = input()
 print("Quantas calorias você ingeriu no café da manhã?")
 calorias_manhã = int(input())
 print("Quantas calorias você ingeriu no almoço?")
@@ -9,6 +9,6 @@ calorias_almoço = int(input())
 print("Quantas calorias você ingeriu no jantar?")
 calorias_jantar = int(input())
 soma = calorias_manhã + calorias_almoço + calorias_jantar
-print(nome + " , você ingeriu nesta " + datetime + " , " + str(soma) + " calorias")
+print(nome + " , você ingeriu nesta " + date + " , " + str(soma) + " calorias!")
 
 
--- README.md
+++ README.md
@@ -5,7 +5,6 @@ I want to know Python Language
 ## :rocket: Starting
 
 
-
 From the Microsoft website, i am starting  my codes in Python.
 
 
