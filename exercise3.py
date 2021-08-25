42fee965a9afacf1e048ee0e5e36fdb95b0ee7a2
SandroRolak <sandro.rolak2090@gmail.com>
2021-08-25 14:46:17 -0300

    Exercise3

---
+++ exercise3.py
@@ -0,0 +1,24 @@
+'''
+medicine = 'Cougjussin'
+dosage = 5
+duration = 4.5
+
+instructions = '{} - Take {} ML by mouth every {}'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{2} - Take {1} ML by mouth every {0}'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{medicine} - Take {dosage} ML by mouth every {duration}'.format(medicine= 'Snnezergen', dosage=10, duration=6)
+print(instructions)
+'''
+
+name = 'World'
+message = f'Hello, {name}.'
+print(message)
+
+count = 10
+value = 3.14
+message = f'Count to {count}.  Multiply by {value}.'
+print(message)
+
